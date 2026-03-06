1f64584f4a82937d69c4e12b97481213fd5ae069
orion.k <o.kabeza@alustudent.com>
2026-03-06 14:19:07 +0200

    Fix newline formatting across all files

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def print_reversed_list_intger(my_list=[]):
-    if my_list is None:
-        return ""
-    else:
-        for num in my_list[::-1]:
-            print("{:d}".format(num))
+def print_reversed_list_integer(my_list=[]):
+    for i in my_list[::-1]:
+        print("{:d}".format(i))
--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = list(my_list)
-    if idx >= 0 and idx < len(new_list):
-        new_list[idx] = element
-        return new_list
-    else:
-        return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
